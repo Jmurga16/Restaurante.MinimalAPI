50a6ab9e3509e2ccbbe89d60bc36faf6c821a8bb
Jose Murga <murga.jose16@gmail.com>
2025-03-28 21:25:12 -0500

    UPDATE PROCEDURES

--- Data/MenuRepository.cs
+++ Data/MenuRepository.cs
@@ -15,12 +15,12 @@ namespace RestauranteMVP.Back.Data
 
         public async Task<IEnumerable<Menu>> GetAllAsync()
         {
-            return await _dbConnection.QueryAsync<Menu>("sp_ObtenerMenus", commandType: CommandType.StoredProcedure);
+            return await _dbConnection.QueryAsync<Menu>("Menu_Obtener", commandType: CommandType.StoredProcedure);
         }
 
         public async Task<Menu> GetByIdAsync(int id)
         {
-            return await _dbConnection.QueryFirstOrDefaultAsync<Menu>("sp_ObtenerMenuPorId", new { MenuId = id }, commandType: CommandType.StoredProcedure)
+            return await _dbConnection.QueryFirstOrDefaultAsync<Menu>("Menu_Obtener_Por_Id", new { MenuId = id }, commandType: CommandType.StoredProcedure)
                 ?? new Menu();
         }
 
@@ -31,13 +31,13 @@ namespace RestauranteMVP.Back.Data
 
         public async Task<bool> UpdateAsync(int id, Menu menu)
         {
-            var result = await _dbConnection.ExecuteAsync("sp_ActualizarMenu", new { menu.Nombre, menu.Descripcion, MenuId = id }, commandType: CommandType.StoredProcedure);
+            var result = await _dbConnection.ExecuteAsync("Menu_Update", new { menu.Nombre, menu.Descripcion, MenuId = id }, commandType: CommandType.StoredProcedure);
             return result > 0;
         }
 
         public async Task<bool> DeleteAsync(int id)
         {
-            var result = await _dbConnection.ExecuteAsync("sp_EliminarMenu", new { MenuId = id }, commandType: CommandType.StoredProcedure);
+            var result = await _dbConnection.ExecuteAsync("Menu_Delete", new { MenuId = id }, commandType: CommandType.StoredProcedure);
             return result > 0;
         }
     }
